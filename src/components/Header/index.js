5403b850218bc5a5b7dc5c2baf4ca5b7e29ceec7
perealvees <perealvees@gmail.com>
2023-08-26 18:43:06 -0300

    criado index.js

---
+++ src/components/Header/index.js
@@ -0,0 +1,11 @@
+function Header (){
+    return (
+        <header>
+            <span>ReactFlix</span>
+            <a href="#">Início</a>
+            <a href="#">Assistir</a>
+        </header>
+    )
+}
+
+export default Header;
