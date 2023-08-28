d3afc9febcda696c33f4fbb7dec02cf9bd0d83d6
perealvees <perealvees@gmail.com>
2023-08-28 12:16:08 -0300

    ajuste footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
         <Header />
         <h1>Catalogo de Filmes</h1>
         <p>Escolha a categoria que você mais ama e bom filme!</p>
+        <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer () {
+    return (
+        <footer className={styles.footer}>
+            <p>Página desenvolvida para aula de aprendizado de React</p>
+        </footer>
+    )
+}
+
+export default Footer;
